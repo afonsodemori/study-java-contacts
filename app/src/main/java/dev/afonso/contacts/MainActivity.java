09721595f2ff8a1523a916cb509a698f975326f5
Afonso de Mori <afonso.mori@gmail.com>
2020-09-06 13:41:18 +0200

    Manipulating view data with TextView.setText

--- app/src/main/java/dev/afonso/contacts/MainActivity.java
+++ app/src/main/java/dev/afonso/contacts/MainActivity.java
@@ -2,16 +2,36 @@ package dev.afonso.contacts;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+
         setContentView(R.layout.activity_main);
+
+        List<String> contacts = new ArrayList<>(Arrays.asList(
+                "Paco",
+                "Quique",
+                "Charo"
+        ));
+
+        TextView contact1 = findViewById(R.id.contact1);
+        TextView contact2 = findViewById(R.id.contact2);
+        TextView contact3 = findViewById(R.id.contact3);
+
+        contact1.setText(contacts.get(0));
+        contact2.setText(contacts.get(1));
+        contact3.setText(contacts.get(2));
     }
 
     // Other tests
