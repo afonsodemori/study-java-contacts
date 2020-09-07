ef30f63d8bca0896ac6588d789dfba56ffea77ae
Afonso de Mori <afonso.mori@gmail.com>
2020-09-07 02:28:18 +0200

    Finish creating Contact functionality and fix Activities flow

--- app/src/main/java/dev/afonso/contacts/ui/ContactCreateActivity.java
+++ app/src/main/java/dev/afonso/contacts/ui/ContactCreateActivity.java
@@ -1,6 +1,5 @@
 package dev.afonso.contacts.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,10 +31,7 @@ public class ContactCreateActivity extends AppCompatActivity {
 
                 ContactDAO.save(contact);
 
-                startActivity(new Intent(
-                        ContactCreateActivity.this,
-                        ContactsListActivity.class
-                ));
+                finish();
             }
         });
     }
--- app/src/main/java/dev/afonso/contacts/ui/ContactsListActivity.java
+++ app/src/main/java/dev/afonso/contacts/ui/ContactsListActivity.java
@@ -1,11 +1,15 @@
 package dev.afonso.contacts.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import dev.afonso.contacts.R;
 import dev.afonso.contacts.dao.ContactDAO;
@@ -16,6 +20,17 @@ public class ContactsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_list);
 
+        ((FloatingActionButton) findViewById(R.id.activity_main_fab_add)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ContactsListActivity.this, ContactCreateActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         ListView contactsList = findViewById(R.id.activity_contacts_list_listView);
         contactsList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ContactDAO.all()));
     }
