64634cd65269ac5ab2b7bcd9dbddd595c3cf041e
Afonso de Mori <afonso.mori@gmail.com>
2020-09-08 03:25:46 +0200

    Use OptionsMenu + some refactoring

--- app/src/main/java/dev/afonso/contacts/ui/ContactCreateUpdateActivity.java
+++ app/src/main/java/dev/afonso/contacts/ui/ContactCreateUpdateActivity.java
@@ -2,9 +2,11 @@ package dev.afonso.contacts.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import dev.afonso.contacts.R;
@@ -20,50 +22,78 @@ public class ContactCreateUpdateActivity extends AppCompatActivity {
 
     private Contact contact;
 
+    private EditText fieldName;
+    private EditText fieldPhone;
+    private EditText fieldEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_create);
-        setTitle(TITLE_BAR_NEW);
+        initializeFields();
+        loadContact();
+    }
 
-        EditText fieldName = findViewById(R.id.activity_contact_create_name);
-        EditText fieldPhone = findViewById(R.id.activity_contact_create_phone);
-        EditText fieldEmail = findViewById(R.id.activity_contact_create_email);
-        Button buttonSubmit = findViewById(R.id.activity_contact_create_button_submit);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_contacts_list_options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.activity_contacts_list_menu_save) {
+            saveForm();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeFields() {
+        fieldName = findViewById(R.id.activity_contact_create_name);
+        fieldPhone = findViewById(R.id.activity_contact_create_phone);
+        fieldEmail = findViewById(R.id.activity_contact_create_email);
+    }
+
+    private void loadContact() {
         Intent intent = getIntent();
         contact = (Contact) intent.getSerializableExtra(KEY_CONTACT);
 
-        if (contact != null) {
+        if (contact == null) {
+            setTitle(TITLE_BAR_NEW);
+            contact = new Contact();
+        } else {
             setTitle(TITLE_BAR_UPDATE);
-            fieldName.setText(contact.getName());
-            fieldPhone.setText(contact.getPhone());
-            fieldEmail.setText(contact.getEmail());
-            buttonSubmit.setText("Update");
+            fillForm();
+        }
+    }
+
+    private void fillForm() {
+        fieldName.setText(contact.getName());
+        fieldPhone.setText(contact.getPhone());
+        fieldEmail.setText(contact.getEmail());
+    }
+
+    private void updateContact() {
+        contact
+                .setName(fieldName.getText().toString())
+                .setPhone(fieldPhone.getText().toString())
+                .setEmail(fieldEmail.getText().toString())
+        ;
+    }
+
+    private void saveForm() {
+        updateContact();
+
+        if (contact.hasValidId()) {
+            ContactDAO.update(contact);
+        } else {
+            try {
+                ContactDAO.save(contact);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
-        buttonSubmit.setOnClickListener(v -> {
-            if (contact == null) {
-                contact = new Contact();
-            }
-
-            contact
-                    .setName(fieldName.getText().toString())
-                    .setPhone(fieldPhone.getText().toString())
-                    .setEmail(fieldEmail.getText().toString())
-            ;
-
-            if (contact.hasValidId()) {
-                ContactDAO.update(contact);
-            } else {
-                try {
-                    ContactDAO.save(contact);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            finish();
-        });
+        finish();
     }
 }
